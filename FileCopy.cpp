9cb123cd5596730f316f738c67d336b058032229
ArinGSA <112737252+ArinGSA@users.noreply.github.com>
2022-09-27 13:31:49 +0530

    Update FileCopy.cpp

--- FileCopy.cpp
+++ FileCopy.cpp
@@ -1,17 +1,29 @@
-#include<stdio.h>
-int main()
-{
-      int ch;
-      FILE *fp,*fq;
-      fp=fopen("AD.txt","r");
-      fq=fopen("AN.txt","w");   
-      if(fp==NULL||fq==NULL)
-            printf("File does not exist");
-      else
-            while((ch=fgetc(fp))!=EOF)
-            {
-                  fputc(ch,fq);
-            }
-      printf("Copied....");
-      return 0;
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+ FILE *fptr1, *fptr2;
+ char filename[100], c;
+ printf("Enter the filename to copy \n");
+ scanf("%s", filename);
+ fptr1 = fopen(filename, "r");
+ if (fptr1 == NULL){
+ printf("Cannot open file %s \n", filename);
+ exit(0);
+ }
+ printf("Enter the filename to paste \n");
+ scanf("%s", filename);
+ fptr2 = fopen(filename, "w");
+ if (fptr2 == NULL) {
+ printf("Cannot open file %s \n", filename);
+ exit(0);
+ }
+ c = fgetc(fptr1);
+ while (c != EOF) {
+ fputc(c, fptr2);
+ c = fgetc(fptr1);
+ }
+printf("\nContents copied to %s", filename);
+ fclose(fptr1);
+ fclose(fptr2);
+ return 0;
 }
